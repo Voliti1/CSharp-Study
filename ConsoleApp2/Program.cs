92e8acec7237bbac54f17d2f6d0e0453e29a85e2
Voliti1 <sjwnick2010@gmail.com>
2026-03-18 14:45:09 +0900

    C# Project revise and creation

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            for (int i = 0; i <= 100; i++)
+            {
+                sum += i;
+            }
+
+            Console.WriteLine("1부터 100까지 더한 값은 : " + sum);
+        }
+    }
+}
