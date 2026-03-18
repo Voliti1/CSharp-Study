d989a3c603d823df5e08eb015a65285219e4f717
Voliti1 <sjwnick2010@gmail.com>
2026-03-18 17:20:58 +0900

    C# Concept Test

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -10,13 +10,37 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
-            int sum = 0;
-            for (int i = 0; i <= 100; i++)
+            int sum = 0; //결과값 누적을 위한 변수 선언
+            for (int i = 0; i <= 100; i++) // 1부터 100까지 반복
             {
+                //sum = sum + i와 같음. 누적합을 구하는 부분
                 sum += i;
             }
 
-            Console.WriteLine("1부터 100까지 더한 값은 : " + sum);
+            Console.WriteLine("1부터 100까지 더한 값은 : " + sum); //결과값 출력
+
+            /* 새로운 챕터 
+             * Console.Write와 
+             * Console.WriteLine을
+             * 비교하기 위한 코드*/
+            Console.Write("Write ");
+            Console.Write("Write ");
+            Console.Write("Write ");
+            Console.WriteLine("Write");
+            Console.WriteLine("Write");
+            Console.WriteLine("Write");
+
+            string hello = "안녕하세요";
+
+            Console.WriteLine(hello[0]);
+            Console.WriteLine(hello[2]);
+            Console.WriteLine(hello[4]);
+
+            bool a = true;
+            bool b = false;
+            Console.WriteLine(a || b);
+            Console.WriteLine(a && b);
+
         }
     }
 }
