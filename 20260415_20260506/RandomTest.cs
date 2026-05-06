a1550881454eafc2edd20d5d590bf653d8c4ba76
1 <12@gmail.com>
2026-05-06 14:39:56 +0900

    20260506 update 1

---
+++ 20260415_20260506/RandomTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20260415_20260506
+{
+    internal class RandomTest
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            Console.WriteLine(random.Next(10));
+            Console.WriteLine(random.Next(10, 100));
+            Console.WriteLine(random.Next(10, 100));
+            Console.WriteLine(random.Next(10, 100));
+            Console.WriteLine(random.Next(10, 100));
+            Console.WriteLine(random.NextDouble());
+        }
+    }
+}
