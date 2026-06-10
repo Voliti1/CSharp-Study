2fe8b625adb8106a6359bc8244d7fcc4f7c4abf9
1 <12@gmail.com>
2026-06-10 17:15:31 +0900

    20260610 update

---
+++ 20260610_WinForm/Form1.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _20260610_WinForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            button1.Click += Button1_Click;
+            FormClosed += Form1_FormClosed;
+
+            timer1.Start();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "+";
+            label1.Text += "+"; 
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {            
+            Button self = (Button)sender;
+            self.Text = "클릭";
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            System.IO.File.AppendAllText("log.txt",
+                $"[{DateTime.Now}] Form1이 닫혔습니다.{Environment.NewLine}");
+
+            timer1.Stop();
+            MessageBox.Show("종료되었습니다", "알림", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private int elapsedTime = 0;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            elapsedTime++;
+            textBox2.Text = elapsedTime + "초 경과";
+            label2.Text = elapsedTime + "초 경과";
+        }
+    }
+}
---
+++ SCT_Form/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using IEG3268_DLL;
+
+namespace SCT_Form
+{
+    public partial class Form1 : Form
+    {
+        IEG3268 EtherCAT_M = new IEG3268();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if(EtherCat_M.CIFX_50RE_Connect() == true)
+            {
+                label2.Text = "OK";
+                EtherCat_M.ReadData_Send_Start(300); //Timer Interval Set
+                EtherCat_M.ReadData_Timer_Start(); //Timer Start
+            }
+            else
+            {
+                label2.Text = "NG";
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            EtherCAT_M.Digital_Output(0, true);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            EtherCAT_M.Digital_Output(0, false);
+        }
+    }
+}
